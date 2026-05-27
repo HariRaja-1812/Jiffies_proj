22ce40dd4d806d6380b4cdb0b5afe9dd609badd8
HARIPRASATH R <hariraja1812@gmail.com>
2026-05-27 09:27:09 +0530

    Add files via upload

--- file/app/app.c
+++ file/app/app.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+int main()
+{
+int fd;
+char buffer[100];
 
+fd = open("/dev/jiffies_dev" ,O_RDONLY);
+
+if (fd < 0)
+{
+perror("open");
+return 1;
+}
+
+read(fd, buffer, sizeof(buffer));	
+
+printf("%s\n" ,buffer);
+
+close(fd);
+
+return 0;
+}
